996dc17a0de42a811198d6236528b7cfc4b48ee3
Kivike <coloris23@gmail.com>
2020-02-23 14:11:41 +0200

    Exercise 6

--- app/src/main/java/fi/oulu/reminder2020/MainActivity.kt
+++ app/src/main/java/fi/oulu/reminder2020/MainActivity.kt
@@ -1,14 +1,20 @@
 package fi.oulu.reminder2020
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import androidx.room.Room
 
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -57,9 +63,40 @@ class MainActivity : AppCompatActivity() {
                     val adapter = ReminderAdapter(applicationContext, reminders)
                     list.adapter = adapter
                 } else {
+                    list.adapter = null
                     toast("No reminders yet")
                 }
             }
+        }
+    }
+
+    companion object {
+        fun showNotification(context: Context, message: String) {
+            val CHANNEL_ID = "REMINDER_NOTIFICATION_CHANNEL"
+            var notificationId = 1589
+            notificationId += Random(notificationId).nextInt(1, 30)
+
+            var notificationBuilder = NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_alarm)
+                .setContentTitle(context?.getString(R.string.app_name))
+                .setContentText(message)
+                .setStyle( NotificationCompat.BigTextStyle().bigText(message))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+            val notificationManager =
+                context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val channel = NotificationChannel(
+                    CHANNEL_ID,
+                    context?.getString(R.string.app_name),
+                    NotificationManager.IMPORTANCE_DEFAULT
+                ).apply {
+                    description = context?.getString(R.string.app_name)
+                }
+                notificationManager.createNotificationChannel(channel)
+            }
+            notificationManager.notify(notificationId, notificationBuilder.build())
         }
     }
 }
